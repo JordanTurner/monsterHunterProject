51ff63182820be3dcbe9d10466661d478fef3d87
JordanTurner <turnerjordan855@gmail.com>
2017-07-02 02:20:11 +0100

    project files

---
+++ inc/config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "safe_user");    // The database username. 
+define("PASSWORD", "BUFIPtC6eCKOCwgv");    // The database password. 
+define("DATABASE", "monster_hunter");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
---
+++ login.php
@@ -0,0 +1,29 @@
+<?php  
+include 'inc/db_connect.php';
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+
+<body>
+<div id ="main">
+
+<div id ="loginForm">
+<form method ="POST">
+<label for="username">Username</label><input type="text" name="username" id="username"><br/>
+<label for="password">Password</label><input type="text" name="password" id="password">
+</form>
+</div>
+
+
+
+
+
+</div> <!--main-->
+
+</body>
+</html>
