c8eb688d253e11e8a088cbeeb445cc6c4f14ce7d
JordanTurner <turnerjordan855@gmail.com>
2017-07-03 19:10:48 +0100

    removed files

--- inc/db_connect.php
+++ inc/db_connect.php
@@ -1,4 +1,4 @@
-<?php
+<?php 
 include_once 'config.php';   // As functions.php is not included
 $mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
 ?>
--- inc/process_login.php
+++ inc/process_login.php
@@ -1,5 +1,5 @@
 <?php
-include_once 'db_connect.php';
+include_once 'db_connect.php'; 
 include_once 'functions.php';
  
 sec_session_start(); // Our custom secure way of starting a PHP session.
@@ -10,7 +10,7 @@ if (isset($_POST['email'], $_POST['p'])) {
  
     if (login($email, $password, $mysqli) == true) {
         // Login success 
-        header('Location: ../protected_page.php');
+        header('Location: ../login_success.php');
     } else {
         // Login failed 
         header('Location: ../index.php?error=1');
