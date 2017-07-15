a3e40b715ee988f0e0898d0868408868b8fbef8c
JordanTurner <turnerjordan855@gmail.com>
2017-07-15 16:09:38 +0100

    Created signout link
    and fixed back for include and redirect in logout.php

--- main.php
+++ main.php
@@ -23,7 +23,7 @@ sec_session_start();
                 the user, so pages will be able to determine the type of user
                 authorised to access the page.
             </p>
-            <p>Return to <a href="index.php">login page</a></p>
+            <p>Return to <a href="logout.php">Signout</a></p>
         <?php else : ?>
             <p>
                 <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
