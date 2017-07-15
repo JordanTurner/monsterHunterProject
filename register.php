a1d8c12d66f7d5f57bd706cb9879d7e71c8859ca
JordanTurner <turnerjordan855@gmail.com>
2017-07-15 23:46:17 +0100

    formatting

--- register.php
+++ register.php
@@ -34,18 +34,12 @@ include_once 'inc/functions.php';
             <li>Your password and confirmation must match exactly</li>
         </ul>
         <?php $add = $_SERVER['REMOTE_ADDR'].$_SERVER['REQUEST_URI']; ?>
-        <form action="<?php echo esc_url($add); ?>" 
-                method="post" 
-                name="registration_form">
+        <form action="<?php echo esc_url($add); ?>" method="post" name="registration_form">
             Username: <input type='text' name='username' id='username' /><br>
             Email: <input type="text" name="email" id="email" /><br>
             Password: <input type="password" name="password" id="password"/><br>
             Confirm password: <input type="password" name="confirmpwd" id="confirmpwd" /><br>
-            <input type="button" value="Register" onclick="return regformhash(this.form,
-                                   this.form.username,
-                                   this.form.email,
-                                   this.form.password,
-                                   this.form.confirmpwd);" /> 
+            <input type="button" value="Register" onclick="return regformhash(this.form, this.form.username, this.form.email, this.form.password, this.form.confirmpwd);" /> 
         </form>
         <p>Return to the <a href="index.php">login page</a>.</p>
     </body>
